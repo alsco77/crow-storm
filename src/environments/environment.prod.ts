bba1852a0fb8159391969fadb04f749ee197947b
Alex Scott <0AlexScott@gmail.com>
2018-05-07 18:15:18 +1000

    Built prod to improve performance

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBdo33SThPU1avpGmcZALK3TQhD1Jk8Urg",
+    authDomain: "mycrypto-web-wallet.firebaseapp.com",
+    databaseURL: "https://mycrypto-web-wallet.firebaseio.com",
+    projectId: "mycrypto-web-wallet",
+    storageBucket: "mycrypto-web-wallet.appspot.com",
+    messagingSenderId: "972262680456"
+  }
 };
